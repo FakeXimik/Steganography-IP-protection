88fad4f27f5e4237418124410397f064f4bed72f
Ximik <brevno5000@gmail.com>
2026-03-07 18:49:40 +0100

    Implemented a simple metadata generator

---
+++ UUID/UserMetadata.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+class UserMetadata:
+
+    """A class to generate and store metadata for a given text and author for later insertion to the image metadata."""
+
+    def __init__(self, author: str, text: str):
+        self.author = author
+        self.data = text
+        self.created_at = datetime.now().isoformat() 
+        self.copyright_info = f"Copyright {datetime.now().year}, All Rights Reserved"
+
+    def get_metadata(self) -> dict:
+        """Returns the generated metadata as a dictionary."""
+
+        return {
+            "author": self.author,
+            "creation_date": self.created_at,
+            "data": self.data
+        }
+    
+    def __str__(self):
+        return f"Here is basic metadata:\n Author: {self.author}\n Creation Date: {self.created_at}\n Data: {self.data}\n Copyright: {self.copyright_info}"
+    
+# -----------------------------------------------
+    
+userData1 = UserMetadata("Sam", "This data is protected because I said so.")
+
+print(userData1)
