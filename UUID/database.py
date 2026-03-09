9663390b06d5f5c22263fc3655c571893e6d011e
Ximik <brevno5000@gmail.com>
2026-03-09 18:34:27 +0100

    Operational retrieval and validation logic (have to fix a bug related to new key-pair generation)

--- UUID/database.py
+++ UUID/database.py
@@ -54,6 +54,7 @@ def save_to_db(user_obj, signed_data: dict):
 
         conn.commit()
         print(f"Metadata saved to DB with UUID: {metadata_uuid}")
+        return metadata_uuid
 
     except psycopg2.IntegrityError as e:
         conn.rollback()
@@ -66,3 +67,59 @@ def save_to_db(user_obj, signed_data: dict):
         if conn:
             cur.close()
             conn.close()
+
+# -------------------------------------
+
+def retrieve_from_db(metadata_uuid: str):
+    conn = None
+
+    try:
+        conn = psycopg2.connect(
+        host = 'localhost',
+        port = 5432,
+        dbname = os.getenv("DB_NAME"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD")
+        )
+
+        cur = conn.cursor()
+
+        fetch_sql = """
+            SELECT
+                m.metadata_content::text,
+                m.signature_hex,
+                u.public_key_hex,
+                u.username
+            FROM 
+                metadata m
+            JOIN users u ON m.user_id = u.id
+            WHERE m.id = %s
+            """
+        
+        cur.execute(fetch_sql, (metadata_uuid,))
+        result = cur.fetchone()
+        
+        if result:
+            json_string, signature_hex, public_key_hex, username = result
+            print(f"Successfully fetched file {metadata_uuid}, (Signed by: {username})")
+
+            return {
+                "json_string": json_string,
+                "public_key_hex": public_key_hex,
+                "signature_hex": signature_hex
+            }
+        else:
+            print(f"No records of {metadata_uuid} found")
+            return None
+        
+    except Exception as e:
+        print(f"--- DB READ ERROR: {e} ---")
+
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+        
+    
+        
+        
--- UUID/main.py
+++ UUID/main.py
@@ -1,26 +1,52 @@
 from models import *
 from crypto import *
 from database import *
+from cryptography.hazmat.primitives import serialization
 
 #----------- basic tests
 
 if __name__ == "__main__":
+    # Generate user + dummy metadata and sign it
     print ('---------------------------------------------------')
-    user1 = User("Cool dude")
+    user1 = User("Cool dude 2")
     print(user1)
     print ('---------------------------------------------------')
     userData1 = UserMetadata("Sam", "This data is protected because I said so.").get_metadata()
     print(userData1)
     print ('---------------------------------------------------')
-
     signed_metadata = proccess_and_sign_metadata(userData1, user1.private_key)
-
     print(signed_metadata)
-    print ('---------------------------------------------------')
-    is_valid = verify_signature(signed_metadata['signature_hex'], user1.public_key, signed_metadata['serialized_data'])
-    print ('---------------------------------------------------')
-    if (is_valid):
-        retrieve_data(signed_metadata['serialized_data'])
-    print ('---------------------------------------------------')
 
-    save_to_db(user1, signed_metadata)
+# Save and retrieve from db
+    print ('---------------------------------------------------')
+    saved_uuid = save_to_db(user1, signed_metadata)
+    
+    if saved_uuid:
+        print("SIGNED METADATA HAS BEEN SAVED TO THE DB")
+
+    print ('---------------------------------------------------')
+    db_data = retrieve_from_db(saved_uuid)
+
+
+    if db_data:
+        print("SIGNED METADATA HAS BEEN RETRIEVED FROM THE DB")
+
+        # Transform from public key from hex and reconstruct json string
+        public_key_bytes = bytes.fromhex(db_data["public_key_hex"])
+        restored_public_key = serialization.load_der_public_key(public_key_bytes)
+
+        import json
+        recovered_dict = json.loads(db_data['json_string'])
+        
+        # Re-stringify it using Python's rules 
+        perfect_json_string = json.dumps(recovered_dict, sort_keys=True)
+
+        print ('---------------------------------------------------')
+        # Verify the integrity
+        is_valid = verify_signature(db_data['signature_hex'], restored_public_key, perfect_json_string)
+        print(db_data['signature_hex'])
+        print ('---------------------------------------------------')
+        if (is_valid):
+            retrieve_data(db_data['json_string'])
+            
+            print ('---------------------------------------------------')
