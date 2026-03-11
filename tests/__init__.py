958392a6bcf3b9376126ba9820b209b58872b91e
Elio V <elio.vidhaj1@proton.me>
2026-03-11 17:40:50 +0100

    Add one test for the cifar100 dataset loader

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 # NOTE: Python treats folders as packages if you have a __init__.py in them.
-#       This is important to avoid path errors when importing our own packages
+#       This is important to avoid path errors when importing our own packages.
 #       Example:
 #           Solved the issue with the line when running pytest:
 #               from UUID import *
---
+++ tests/test_DataLoader.py
@@ -0,0 +1,15 @@
+import pytest
+import torch
+
+from DataLoader import CIFAR100
+
+def test_data():
+    cifar = CIFAR100() # Default download folder is #root#/Assets/cifar-100
+
+    train_loader = cifar.train_loader
+
+    train_iter = iter(train_loader)
+    train_imgs, train_labels = next(train_iter)
+
+    assert train_imgs.shape == torch.Size([64, 3, 32, 32])
+    assert train_labels.shape == torch.Size([64])
